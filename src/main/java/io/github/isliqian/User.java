98760053ba83d3f78694f814676275f2d8897c49
isliqian <51103942@qq.com>
2018-09-05 23:58:19 +0800

    添加lombok

--- src/main/java/io/github/isliqian/User.java
+++ src/main/java/io/github/isliqian/User.java
@@ -1,9 +1,11 @@
 package io.github.isliqian;
 
+import lombok.Data;
+
 import java.io.Serializable;
-import java.util.Date;
 
 
+@Data
 public class User implements Serializable{
 
 	private Integer id;
@@ -13,43 +15,4 @@ public class User implements Serializable{
 	private Boolean state;
 
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getNow() {
-		return now;
-	}
-
-	public void setNow(String now) {
-		this.now = now;
-	}
-
-	public Boolean getState() {
-		return state;
-	}
-
-	public void setState(Boolean state) {
-		this.state = state;
-	}
 }
